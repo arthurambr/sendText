2c58fe6b0ff59aadc6d664ea13b0db7d2c5fcc45
Arthur Mendes <arthur@Arthurs-Mac.local>
2017-01-19 20:36:57 -0200

    Change import and class statements

--- send_text.py
+++ send_text.py
@@ -1,13 +1,13 @@
-from twilio.rest import TwilioRestClient
+from twilio import rest
 
 account_sid = "AC3c9a371a25ea0df7407919853ae16239" # Your Account SID from www.twilio.com/console
 auth_token  = "8bb20f348564ba4e26e92f1548b78fac"  # Your Auth Token from www.twilio.com/console
 
-client = TwilioRestClient(account_sid, auth_token)
+client = rest.TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(
     to="+5531993885379", 
     from_="+14152003426",
-    body="Ich bin Harry Potter!")
+    body="After some changes!")
 
 print(message.sid)
